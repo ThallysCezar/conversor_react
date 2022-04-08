99787515659abd636edc502a70b2c6c2bae2174f
Thallys <thalys_cezar@hotmail.com>
2022-04-08 12:56:43 -0300

    refactor: adicionando arquivos conversor.js e conversor.css + pasta

--- src/componentes/Conversor.js
+++ src/componentes/Conversor.js
@@ -4,13 +4,22 @@ export default class Conversor extends Component {
   constructor(props) {
     super(props);
     this.state = {
+        moedaA_valor: "",
+        moedaB_valor: 0,
     };
+  }
+
+  converter(){
+
   }
 
   render() {
     return (
       <div className='conversor'>
-        
+        <h2>{this.props.moedaA} para {this.props.moedaB}</h2>
+        <input type="text" onChange={(event)=>{this.setStage({moedaA_valor: event.target.value})}}></input>
+        <input type="button" value="Converter" oncClick={this.converter}></input>
+        <h2>Valor convertido</h2>
       </div>
     );
   }
